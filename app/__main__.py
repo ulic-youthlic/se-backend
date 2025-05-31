d6c30e3398d374b5ff30bf3842199eb629276e7b
ulic-youthlic <ulic.youthlic@gmail.com>
2025-05-31 14:22:09 +0800

    add prefix for api path

--- app/__main__.py
+++ app/__main__.py
@@ -9,7 +9,7 @@ api_router.include_router(login.router, prefix="/login")
 api_router.include_router(autoaim.router, prefix="/autoaim")
 
 app = FastAPI()
-app.include_router(api_router)
+app.include_router(api_router, prefix="/api")
 
 
 def main():
